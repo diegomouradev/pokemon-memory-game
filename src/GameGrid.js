1aaabe3cf64c5cf6135ef11062f2519d988ec133
Diego Moura <59176835+diegomouradev@users.noreply.github.com>
2021-05-30 19:25:31 -0400

    matching cards implemented

--- src/GameGrid.js
+++ src/GameGrid.js
@@ -1,14 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCard from "./PokemonCard";
 import "./GameGrid.css";
 
 export default function GameGrid(props) {
   const pokeData = props.pokeData;
+  const [cardsSelected, setCardsSelected] = useState([]);
+
+  useEffect(() => {
+    if (cardsSelected.length === 2 && cardsSelected[0] === cardsSelected[1]) {
+      setCardsSelected([]);
+      setTimeout(() => {
+        alert(`Yay! you found a pair!`);
+      }, 100);
+    } else if (cardsSelected.length === 2) {
+      setCardsSelected([]);
+      setTimeout(() => {
+        alert(`Try again!`);
+      }, 100);
+    }
+  }, [cardsSelected.length, cardsSelected]);
 
   return (
     <div className="game-grid">
-      <PokemonCard card={1} pokeData={pokeData} />
-      <PokemonCard card={2} pokeData={pokeData} />
+      <PokemonCard
+        card={1}
+        pokeData={pokeData}
+        setCardsSelected={setCardsSelected}
+        cardsSelected={cardsSelected}
+      />
+      <PokemonCard
+        card={2}
+        pokeData={pokeData}
+        setCardsSelected={setCardsSelected}
+        cardsSelected={cardsSelected}
+      />
     </div>
   );
 }
--- src/PokemonCard.js
+++ src/PokemonCard.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import "./PokemonCard.css";
 
 const PokemonCard = (props) => {
+  const { card, setCardsSelected, cardsSelected } = props;
   const [showCard, setShowCard] = useState(Array(20).fill(false));
 
-  const handleClick = (index) => {
+  const handleClick = (index, pokemon) => {
     showCard[index] = !showCard[index];
-    let newState = [...showCard];
-    setShowCard(newState);
+    let newCardsSelected = [...cardsSelected, pokemon.name];
+    setCardsSelected(newCardsSelected);
+    setShowCard([...showCard]);
   };
 
   return (
@@ -16,11 +18,11 @@ const PokemonCard = (props) => {
         return (
           <div
             key={index}
-            id={`card-${props.card}-poke${index}`}
+            id={`card-${card}-poke${index}`}
             className={
               "card " + (showCard[index] ? "poke-card-show" : "poke-card-hide")
             }
-            onClick={() => handleClick(index)}
+            onClick={() => handleClick(index, pokemon)}
           >
             <div className="poke-card poke-card--front">
               <div>Pokemon</div>
